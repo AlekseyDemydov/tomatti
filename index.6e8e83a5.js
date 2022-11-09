3dd69a9e4cc51d2d070b2bac846b4030a62b7476
AlekseyDemydov <96746949+AlekseyDemydov@users.noreply.github.com>
2022-11-09 23:48:59 +0000

    Deploying to gh-pages from @ AlekseyDemydov/tomatti@13eccf8481a37ae2d555af138104ec540f0d9d43 🚀

---
+++ index.6e8e83a5.js
@@ -0,0 +1,2 @@
+$((function(){$("#nav-icon3").click((function(){$(this).toggleClass("open")}))}));const n=document.getElementById("nav-icon3"),e=document.getElementById("nav-overlay");n.addEventListener("click",(()=>{e.classList.toggle("hide")}));
+//# sourceMappingURL=index.6e8e83a5.js.map
---
+++ index.b0e6cffc.js
@@ -0,0 +1,2 @@
+!function(){$((function(){$("#nav-icon3").click((function(){$(this).toggleClass("open")}))}));var n=document.getElementById("nav-icon3"),e=document.getElementById("nav-overlay");n.addEventListener("click",(function(){e.classList.toggle("hide")}))}();
+//# sourceMappingURL=index.b0e6cffc.js.map
